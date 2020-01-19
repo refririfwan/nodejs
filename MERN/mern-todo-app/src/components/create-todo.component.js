e1a57b6e30dbcfadc4d29ba63ac66839eb9eb860
Refri Rifwan <refririfwan@gmail.com>
2020-01-19 17:50:29 +0700

    MERN Part 3

--- MERN/mern-todo-app/src/components/create-todo.component.js
+++ MERN/mern-todo-app/src/components/create-todo.component.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 
 export default class CreateTodo extends Component {
 
@@ -14,7 +15,7 @@ export default class CreateTodo extends Component {
             todo_description: '',
             todo_responsible: '',
             todo_priority: '',
-            todo_compeleted: false
+            todo_completed: false
         }
     }
 
@@ -43,13 +44,22 @@ export default class CreateTodo extends Component {
         console.log((`Todo Descripton: ${this.state.todo_description}`));
         console.log((`Todo Responsible: ${this.state.todo_responsible}`));
         console.log((`Todo Priority: ${this.state.todo_priority}`));
-        console.log((`Todo Completed: ${this.state.todo_compeleted}`));
+        console.log((`Todo Completed: ${this.state.todo_completed}`));
+
+        const newTodo = {
+            todo_description: this.state.todo_description,
+            todo_responsible: this.state.todo_responsible,
+            todo_priority: this.state.todo_priority,
+            todo_completed: this.state.todo_completed
+        }
+
+        axios.post('http://localhost:4000/todos/add', newTodo).then(res => console.log(res.data))
 
         this.setState({
             todo_description: '',
             todo_responsible: '',
             todo_priority: '',
-            todo_compeleted: false
+            todo_completed: false
         })
     }
 
--- MERN/mern-todo-app/src/components/todos-list.component.js
+++ MERN/mern-todo-app/src/components/todos-list.component.js
@@ -1,10 +1,55 @@
 import React, {Component} from 'react'
+import {Link}  from 'react-router-dom'
+import axios from  'axios'
+
+const Todo = props => (
+    <tr>
+        <td>{props.todo.todo_description}</td>
+        <td>{props.todo.todo_responsible}</td>
+        <td>{props.todo.todo_priority}</td>
+        <td> <Link to={"/edit/"+props.todo_id}> Edit </Link></td>
+    </tr>
+)
 
 export default class TodosList extends Component {
+    constructor(props){
+        super(props)
+        this.state = {todos: []}
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:4000/todos')
+        .then(res => {
+            this.setState({todos: res.data})
+        })
+        .catch(function(err){
+            console.log(err)
+        })
+    }
+
+    todosList(){
+        return this.state.todos.map(function(currrentTodo, i){
+            return <Todo todo={currrentTodo} key={i} />
+        })
+    }
+
     render () {
         return (
             <div>
-                <p>Welcome Todos List Component!</p>
+                <h3>Todos List</h3>
+                <table className="table table-stripped" style={{marginTop:20}}>
+                    <thead>
+                        <tr>
+                            <th> Description </th>
+                            <th> Responsible </th>
+                            <th> Priority </th>
+                            <th> Actions </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.todosList() }
+                    </tbody>
+                </table>
             </div>
         )
     }
