23dc4ce077fe437b9025702aae33b28d000566d7
Refri Rifwan <refririfwan@gmail.com>
2020-02-07 21:44:23 +0700

    update ui link

--- koa-adminlte/index.js
+++ koa-adminlte/index.js
@@ -25,9 +25,59 @@ render(app, {
 })
 
 // Routes
+// dashboard
 router.get('/dashboard', async ctx => {
     await ctx.render('dashboard', {
-        title: "Dashboard"
+        title: "Dashboard",
+        link: "dashboard"
+    })
+})
+
+// device
+router.get('/device', async ctx => {
+    await ctx.render('device', {
+        title: "Device",
+        link: "device"
+    })
+})
+
+// data bucket
+router.get('/dataBucket', async ctx => {
+    await ctx.render('dataBucket', {
+        title: "Data Bucket",
+        link: "dataBucket"
+    })
+})
+
+// End Point
+router.get('/endpoints', async ctx => {
+    await ctx.render('endpoints', {
+        title: "Endpoints",
+        link: "endpoints"
+    })
+})
+
+// Profile
+router.get('/profile', async ctx => {
+    await ctx.render('profile', {
+        title: "Profile",
+        link: "profile"
+    })
+})
+
+// Setting
+router.get('/setting', async ctx => {
+    await ctx.render('setting', {
+        title: "Setting",
+        link: "setting"
+    })
+})
+
+// Access Token
+router.get('/accessToken', async ctx => {
+    await ctx.render('accessToken', {
+        title: "Access Token",
+        link: "accessToken"
     })
 })
 
