836d597d6d97cc3eef48ab3e52dcca01c42f1f4c
Refri Rifwan <refririfwan@gmail.com>
2020-02-12 10:55:51 +0700

    login not finished

---
+++ express-login/server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express()
+
+const users = []
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({extended:false}))
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', [name='Refri'])
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/login', (req, res) => {
+
+})
+
+app.post('/register', (req, res) => {
+
+})
+
+app.listen(3000)
